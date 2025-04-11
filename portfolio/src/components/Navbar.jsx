8c837596639603ae8372cb494e08f94175932cd5
ARVIND-46 <114347982+ARVIND-46@users.noreply.github.com>
2025-04-11 17:21:08 +0530

    make it Responsive

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import '../components/styles.css'
+import React, { useState } from 'react';
+import '../components/styles.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
-    <div className="navbar">
-        <nav>
-            <ul>
-                <li><a href="#Home">Home</a></li>
-                <li><a href="#About">About</a></li>
-                <li><a href="#Skills">skills</a></li>
-                <li><a href="#Project">Projects</a></li>
-                <li><a href="#Contact">Contact</a></li>
-            </ul>
-        </nav>
-       
-    </div>
-    </>
-  )
-}
+      <div className="navbar">
+        <div className="hamburger" onClick={toggleMenu}>
+          ☰
+        </div>
 
-export default Navbar
+        <nav className={`nav-menu ${menuOpen ? 'open' : ''}`}>
+          <ul>
+            <li><a href="#Home">Home</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Skills">Skills</a></li>
+            <li><a href="#Project">Projects</a></li>
+            <li><a href="#Contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
